eb072f5e98e5505d0355ecc50b4ec5a763148200
Chris Oliver <excid3@gmail.com>
2010-11-15 21:35:48 -0600

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,62 @@
+class TasksController < ApplicationController
+  # GET /tasks
+  # GET /tasks.xml
+  def index
+    @task = Task.new
+    @tasks = Task.order("updated_at DESC")
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @tasks }
+    end
+  end
+
+  # GET /tasks/1/edit
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  # POST /tasks
+  # POST /tasks.xml
+  def create
+    @task = Task.new(params[:task])
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to(tasks_path, :notice => 'Task was successfully created.') }
+        format.xml  { render :xml => @task, :status => :created, :location => @task }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @task.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /tasks/1
+  # PUT /tasks/1.xml
+  def update
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      if @task.update_attributes(params[:task])
+        format.html { redirect_to(@task, :notice => 'Task was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @task.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tasks/1
+  # DELETE /tasks/1.xml
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(tasks_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fluttr::Application.config.secret_token = '6d219a57c95dfc3c1d9cace9b7f11c1de21a4890782b585445322735d9fa594beb3ea605429734a8e4100ef83f009120f3ea7ecbf157f4f463222e39e2375dff'
