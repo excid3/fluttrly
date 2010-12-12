51c77708224412e3667be45eb79edef296ac997c
Chris Oliver <excid3@gmail.com>
2010-12-11 21:11:22 -0600

    Moved strikeout to client side

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,12 +2,20 @@
 // This file is automatically included by javascript_include_tag :defaults
 document.observe("dom:loaded", function () {
   $("tasks").observe("click", function(e) {
-	if (Event.element(e).nodeName == "INPUT") {
-      var id = Event.element(e).up().identify();
-	  new Ajax.Request("/tasks/" + id, {
-        method: 'put',
-		parameters: { completed: Event.element(e).checked }
-      });
+      var c = Event.element(e);
+      if (c.nodeName == "INPUT") {
+	
+	if (c.checked) {
+	  c.next().addClassName("completed");
+	} else {
+	  c.next().removeClassName("completed");
 	}
+
+	var id = c.up().identify();
+	new Ajax.Request("/tasks/" + id, {
+	  method: 'put',
+	  parameters: { completed: c.checked }
+	});
+      }
   });
 });
