c531a693e75188052170cfd8729f476445391567
Chris Oliver <excid3@gmail.com>
2010-12-19 13:12:24 -0600

    Removed some share options, set focus on input

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 document.observe("dom:loaded", function () {
+  $("task_content").focus();
+
   $("tasks").observe("click", function(e) {
       var c = Event.element(e);
       if (c.nodeName == "INPUT") {
