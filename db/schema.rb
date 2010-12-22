41853f98d96f166e772d08850db603851a429f8a
Chris Oliver <excid3@gmail.com>
2010-12-22 00:03:53 -0600

    Ordering of results SMS

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101212010945) do
+ActiveRecord::Schema.define(:version => 20101222054742) do
 
   create_table "tasks", :force => true do |t|
-    t.string   "name"
-    t.string   "content"
+    t.string   "name",                          :null => false
+    t.string   "content",                       :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "completed",  :default => false
+    t.boolean  "completed",  :default => false, :null => false
   end
 
 end
