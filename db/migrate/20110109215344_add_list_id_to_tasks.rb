9bd48cd0bbdf08039b165d162f21cc9b371ec2c9
Benson Kalahar <bensonk42@gmail.com>
2011-01-10 00:49:25 -0800

    Added smart migration to create lists on the fly.

--- db/migrate/20110109215344_add_list_id_to_tasks.rb
+++ db/migrate/20110109215344_add_list_id_to_tasks.rb
@@ -1,8 +1,15 @@
 class AddListIdToTasks < ActiveRecord::Migration
   def self.up
     add_column :tasks, :list_id, :integer
+    Task.all.each do |t|
+      list = List.find_or_create_by_name t.name
+      list.save
+      t.list = list
+      t.save
+    end
   end
 
   def self.down
+    remove_column :tasks, :list_id, :integer
   end
 end
