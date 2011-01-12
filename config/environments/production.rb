7cc9c5cc9a9b45999c5aa13ad3a37b97c50f50c8
Chris Oliver <excid3@gmail.com>
2011-01-11 19:08:47 -0600

    Readded default mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Fluttr::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
+  config.action_mailer.default_url_options = { :host => "fluttr.heroku.com" }
+
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
