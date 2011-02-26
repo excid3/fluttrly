8f191ba594db1a85704e68ca2dc1bbe7d7695352
Chris Oliver <excid3@gmail.com>
2011-02-25 21:11:43 -0600

    Cleaned up routes and updated schema for public lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Fluttr::Application.routes.draw do
   devise_for :users, :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "signup" }
   match "/users" => redirect("/users/edit")
 
+  resources :tasks
   match 'sms', :to => "tasks#sms", :via => :post
   match 'features', :to => "tasks#features", :as => "features"
   match ':name/lock', :to => "tasks#lock", :via => :post
-
-  resources :tasks
-  
   match ':name', :to => "tasks#index", :as => "index"
 
   root :to => "tasks#home"
