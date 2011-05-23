e4adcb7c9b35d99489e743b10e67e9f7fce619ba
Danny Chrastil <danny@devtrixlabs.com>
2011-05-23 11:21:35 -0700

    Cleaning up

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,9 +1,8 @@
 module TasksHelper
   def create_links(content)
-    content.gsub!(/#([^\s]+)/) do |match|
-      link_to "##{h($1)}", "/#{$1}"
+    content = h(content)
+    content.gsub(/#([^\s]+)/) do |match|
+      link_to "##{$1}", "/#{$1}"
     end
-
-    return content.html_safe
   end
 end
