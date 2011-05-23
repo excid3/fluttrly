b7468ccd3ab34052a2528ce1560a2872eaf44953
Chris Oliver <excid3@gmail.com>
2011-05-23 13:35:16 -0500

    Clean up helper

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,8 +1,5 @@
 module TasksHelper
   def create_links(content)
-    content = h(content)
-    content.gsub(/#([^\s]+)/) do |match|
-      link_to "##{$1}", "/#{$1}"
-    end
+    h(content).gsub(/#([^\s]+)/) { |match| link_to "##{$1}", "/#{$1}" }
   end
 end
