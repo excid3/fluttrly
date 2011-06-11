ce1c432ee0742e5b5b68ae26c955a1b90b81afff
Chris Oliver <excid3@gmail.com>
2011-06-11 11:26:44 -0500

    Updated gems

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.1'
-gem 'devise', '1.1.5'
+gem 'rails', '3.0.8'
+gem 'devise', '1.3.4'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- db/schema.rb
+++ db/schema.rb
@@ -21,28 +21,27 @@ ActiveRecord::Schema.define(:version => 20110226212932) do
   end
 
   create_table "tasks", :force => true do |t|
-    t.string    "name",                          :null => false
-    t.string    "content",                       :null => false
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.boolean   "completed",  :default => false, :null => false
-    t.integer   "list_id"
+    t.string   "name",                          :null => false
+    t.string   "content",                       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "completed",  :default => false, :null => false
+    t.integer  "list_id"
   end
 
   create_table "users", :force => true do |t|
-    t.string    "email",                               :default => "", :null => false
-    t.string    "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string    "password_salt",                       :default => "", :null => false
-    t.string    "reset_password_token"
-    t.string    "remember_token"
-    t.timestamp "remember_created_at"
-    t.integer   "sign_in_count",                       :default => 0
-    t.timestamp "current_sign_in_at"
-    t.timestamp "last_sign_in_at"
-    t.string    "current_sign_in_ip"
-    t.string    "last_sign_in_ip"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.string   "remember_token"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
